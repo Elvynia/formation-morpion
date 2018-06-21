5d1ee2bcbacd62c69fbd62474604d5757e727940
Elvynia <elvynia@gmail.com>
2018-06-21 17:12:58 +0200

    Mise en place de la structure de base pour gérer un jeu du Morpion. Ajout des règles CSS correspondants aux symboles des joueurs.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,33 @@
-console.log('Démarrage du Morpion !');
+// Utilisation d'une IIFE (Immediately invoked function expression).
+(function() {
+	// L'objet console permet de sortir des logs et des informations
+	// détaillée sur les objets du DOM.
+	console.log('Démarrage du Morpion !');
+
+	let Player = function(username, className) {
+		this.username = username;
+		this.className = className;
+		this.index = null;
+		this.getName = function() {
+			return this.username;
+		};
+	};
+
+	let Game = function() {
+		this.turnCount = 0;
+		this.players = [
+			new Player('Joueur 1', 'circle'),
+			new Player('Joueur 2', 'cross')
+		];
+		this.data = [
+			[], [], []
+		];
+		this.playing = false;
+		this.initialize = function() {
+			// Récupérer chaque td de la table et associer
+			// une fonction play() sur l'événement 'click'.
+		};
+	};
+
+	window.hyperpion = new Game();
+})();
