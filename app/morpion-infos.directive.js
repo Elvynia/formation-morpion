345775aa462c1315203cc4e005b34d97c2e33d7f
Elvynia <elvynia@gmail.com>
2018-06-21 10:10:29 +0200

    Reset des sources : base HTML/CSS avec Jquery et Bootstrap importé via CDN.

--- app/morpion-infos.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('Morpion').directive('morpionInfos', function() {
-	return {
-		restrict: 'A',
-		scope: {
-			'currentPlayer': '=morpionInfos'
-		},
-		templateUrl: 'morpion-infos.template.html'
-	};
-});
