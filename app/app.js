897af7ddc358ebbbb7fc9a37d0c1d4f175f35e71
Elvynia <elvynia@gmail.com>
2018-06-22 12:01:23 +0200

    Début de l'implémentation de la partie 2.

--- app/app.js
+++ app/app.js
@@ -24,8 +24,44 @@
 		];
 		this.playing = false;
 		this.initialize = function() {
-			// Récupérer chaque td de la table et associer
-			// une fonction play() sur l'événement 'click'.
+			// Récupérer chaque td de la table
+			let cells = document.querySelectorAll('table.morpion td');
+			cells.forEach((cell) => {
+				// Associer une fonction play() sur l'événement 'click'.
+				cell.addEventListener('click', this.play);
+			});
+		};
+		this.play = function(event) {
+			// ATTENTION : cette fonction n'est pas exécutée dans le même contexte que les autres.
+			// En effet la fonction play n'est pas appelée par "notre JavaScript" mais par le
+			// navigateur lui-même lorsqu'un clic est détécté sur l'élément écouté.
+			// Récupération du joueur courant avec le nombre de tours et le tableau de joueurs.
+			// L'utilisation de l'opérateur modulo ('%') permet de boucler les indices
+			// à l'intérieur du tableau.
+			// Exemples :
+			// 		turnCount = 0; (turnCount % 2) -> 0
+			// 		turnCount = 1; (turnCount % 2) -> 1
+			// 		turnCount = 2; (turnCount % 2) -> 0
+			// 		turnCount = 3; (turnCount % 2) -> 1
+			// 		turnCount = 4; (turnCount % 2) -> 0
+			let currentPlayer = hyperpion.players[hyperpion.turnCount % 2];
+			// Bonne pratique : récupérer la cible de l'événement par 'target'
+			// ou sinon (si target null ou undefined) utiliser currentTarget.
+			let clickedCell = event.target || event.currentTarget;
+			console.log('Le joueur %s a joué dans la cellule :', currentPlayer.getName(), clickedCell);
+			// Ajouter le symbole du joueur dans la cellule et remplir les données métier 'data'.
+
+			// Vérifier si le joueur a gagné la partie
+
+			// Si la partie continue : Incrémenter le nombre de tours, retirer le listener sur 'click'.
+			// Bonne pratique dans tous les langages : préférer la pré-incrémentation si possible.
+			++hyperpion.turnCount;
+		};
+		this.checkVictory = function() {
+			
+		};
+		this.stopGame = function(winner) {
+
 		};
 	};
 
